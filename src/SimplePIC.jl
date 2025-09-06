bcb6b2c8653d2ef39f9806622858d9a8f077a143
Josef Ferda <josef.ferda@hotmail.cz>
2025-09-06 23:17:10 +0200

    k_B

--- src/SimplePIC.jl
+++ src/SimplePIC.jl
@@ -14,6 +14,7 @@ const epsilon_0 = 8.8541878128e-12
 const e = 1.60217662e-19
 const m_e = 9.10938356e-31
 const m_p = 1.6726219e-27
+const k_B = 1.380649e−23
 
 export m_e, q_e, amu, k_B
 export NeutralEnsemble, ParticleEnsemble, Particle1d3v, Particle3d3v, Particle0d3v, Particle1d3vE, Particle1d1vE
